6810ddf932d6f18d24c5e152edbaddcbcb3d8ba4
Dimanda <traperwaze@gmail.com>
2023-06-16 08:52:24 +0700

    add json tags

--- entity/entity.go
+++ entity/entity.go
@@ -1,10 +1,10 @@
 package entity
 
 type User struct {
-	ID       int `storm:"id,increment"`
-	Fullname string
-	Username string `storm:"unique"`
-	Password string
+	ID       int    `storm:"id,increment" json:"id"`
+	Fullname string `json:"fullname"`
+	Username string `storm:"unique" json:"username"`
+	Password string `json:"password"`
 }
 
 type Question struct {
@@ -13,22 +13,22 @@ type Question struct {
 }
 
 type Choice struct {
-	ID         int `storm:"id,increment" json:"id"`
-	QuestionID int
+	ID         int    `storm:"id,increment" json:"id"`
+	QuestionID int    `json:"questionId"`
 	Body       string `json:"body"`
-	Correct    bool
+	Correct    bool   `json:"correct"`
 }
 
 type Answer struct {
-	ID         int `storm:"id,increment"`
-	UserID     int `storm:"index"`
-	QuestionID int `storm:"index"`
-	ChoiceID   int `storm:"index"`
-	Correct    bool
+	ID         int  `storm:"id,increment" json:"id"`
+	UserID     int  `storm:"index" json:"userId"`
+	QuestionID int  `storm:"index" json:"questionId"`
+	ChoiceID   int  `storm:"index" json:"choiceId"`
+	Correct    bool `json:"correct"`
 }
 
 type Score struct {
-	ID     int `storm:"id,increment"`
-	UserID int `storm:"index,unique"`
-	Value  float64
+	ID     int     `storm:"id,increment" json:"id"`
+	UserID int     `storm:"index,unique" json:"userId"`
+	Value  float64 `json:"value"`
 }
