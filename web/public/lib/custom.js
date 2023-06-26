d007c06ea6fce8830368fc12c945360632a370fa
Dimanda <traperwaze@gmail.com>
2023-06-26 20:28:17 +0700

    fix: dashboard not updated correctly

--- web/public/lib/custom.js
+++ web/public/lib/custom.js
@@ -4,46 +4,54 @@ const [, ...tblRows] = theTable.rows;
 
 let pending = false;
 
-btnRefresh.addEventListener('click', async () => {
-    if (pending) return;
+const update = async () => {
+    try {
+        if (pending) return;
 
-    pending = true;
-    const res = await fetch('/adm/raw');
-    const data = await res.json();
+        pending = true;
+        const res = await fetch('/adm/raw');
+        const data = await res.json();
 
-    data.students.forEach((student, i) => {
-        const cellState = tblRows[i].children[2];
-        const cellScore = tblRows[i].children[3];
+        data.students.forEach((student, i) => {
+            const cellState = tblRows[i].children[2];
+            const cellScore = tblRows[i].children[3];
 
-        const elState = document.createElement('span');
-        elState.classList.add('badge');
-        elState.textContent = student.status;
+            const elState = document.createElement('span');
+            elState.classList.add('badge');
+            elState.textContent = student.status;
 
-        switch (student.status) {
-            case 'Working':
-                elState.classList.add('secondary');
-                break;
-            case 'Online':
-                elState.classList.add('success');
-                break;
-        }
+            switch (student.status) {
+                case 'Working':
+                    elState.classList.add('secondary');
+                    break;
+                case 'Online':
+                    elState.classList.add('success');
+                    break;
+            }
 
-        cellState.replaceChildren(elState);
-        if (!cellScore.textContent) {
+            cellState.replaceChildren(elState);
             cellScore.textContent = student.score?.value;
-        }
 
-        if (!tblRows[i].children[4]) {
+            const frontIndexLimit = 3;
+
             student.answers.forEach(answer => {
-                const cellAnswer = document.createElement('td');
-                cellAnswer.textContent = answer.correct ? '🟢' : '🔴';
-                tblRows[i].append(cellAnswer);
-            });
-        }
-    });
-    pending = false;
-});
+                if (answer.id == 0) {
+                    return
+                }
 
+                const text = answer.correct ? '🟢' : '🔴';
+
+                tblRows[i].children[frontIndexLimit + answer.questionId].textContent = text
+            });
+        });
+        pending = false;
+    }
+    catch {
+        pending = false
+    }
+}
+
+btnRefresh.addEventListener('click', update);
 setInterval(() => {
-    btnRefresh.click();
+    update()
 }, 1000);
