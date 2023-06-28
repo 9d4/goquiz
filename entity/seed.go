ef43b85ae052a77decc479d0123f97478cb3f631
Dimanda <traperwaze@gmail.com>
2023-06-28 10:25:16 +0700

    remove seed by yml

--- entity/seed.go
+++ entity/seed.go
@@ -29,15 +29,7 @@ func Seed() {
 
 	SeedUsers(reader.AllSettings())
 
-	reader.SetConfigName("quiz")
-	err = reader.ReadInConfig()
-	if err != nil {
-		log.Println("Unable to seed quiz,", err)
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			log.Println("Make sure to create quiz.yml; See quiz.example.yml for example")
-		}
-	}
-	SeedQuestions(reader.AllSettings())
+	SeedQuestionExcel()
 
 	log.Println("Seeding...done")
 }
@@ -85,81 +77,6 @@ func SeedUsers(data map[string]interface{}) {
 
 		user := &User{Fullname: fullname, Username: username, Password: password}
 		DB().Save(user)
-	}
-}
-
-func SeedQuestions(data map[string]interface{}) {
-	name, ok := data["name"].(string)
-	if !ok {
-		log.Printf("Invalid quiz name, should be string; got %#v", data["name"])
-		return
-	}
-	SaveQuizName(name)
-
-	ref := reflect.TypeOf(data["questions"])
-	if ref.Kind() != reflect.Slice {
-		log.Printf("Invalid questions, should be array of question")
-		return
-	}
-
-	questions := data["questions"].([]interface{})
-	for _, q := range questions {
-		q, ok := q.(map[string]interface{})
-		if !ok {
-			continue
-		}
-
-		body, ok := q["body"].(string)
-		if !ok {
-			continue
-		}
-
-		question := &Question{
-			Body: body,
-		}
-		tx, err := DB().Begin(true)
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-
-		if err := tx.Save(question); err != nil {
-			log.Println(err)
-			continue
-		}
-
-		if reflect.TypeOf(q["choices"]).Kind() != reflect.Slice {
-			tx.Rollback()
-			continue
-		}
-
-		choices, ok := q["choices"].([]interface{})
-		if !ok {
-			tx.Rollback()
-			continue
-		}
-
-		for _, c := range choices {
-			ch, ok := c.(map[string]interface{})
-			if !ok {
-				continue
-			}
-
-			body, ok := ch["body"].(string)
-			if !ok {
-				continue
-			}
-			correct, _ := ch["correct"].(bool)
-
-			choice := &Choice{
-				QuestionID: question.ID,
-				Body:       body,
-				Correct:    correct,
-			}
-			tx.Save(choice)
-		}
-
-		tx.Commit()
 	}
 }
 
